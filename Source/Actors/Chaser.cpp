b7ca94bf50c5b031e702936834db81e2a407b89a
Joaozinho nas baixuras <pessoa.pessoinha.pequena@gmail.com>
2025-11-30 20:45:44 -0300

    nerfed missile

--- Source/Actors/Chaser.cpp
+++ Source/Actors/Chaser.cpp
@@ -25,8 +25,8 @@ Chaser::Chaser(Game* game, float moveSpeed, int maxHitPoints)
     , mMode(ChaserMode::Following)
     , mCooldownTimer(0.0f)
     , mMissileFireTimer(0.0f)
-    , mMissileFireRate(6.0f)
-    , mBattleMissileFireRate(2.0f)
+    , mMissileFireRate(7.0f)
+    , mBattleMissileFireRate(3.5f)
     , mLastPlayerPosition(Vector2::Zero)
 {
     // death frames: 0,2,4,25,26,27,30,31  | 3fire frames: 1,3,5,6,7,8,9,10,12,13,14,15,17,18,19,20 | forward frames: 11,15,16,21
@@ -100,7 +100,7 @@ void Chaser::UpdateSteering(float deltaTime)
     if (mMissileFireTimer >= mMissileFireRate)
     {
         // Normal missile
-        new Missile(GetGame(), mPosition, 150.0f, 5.0f, "../Assets/Sprites/Chaser/roc.png", Vector3(1.0f, 0.5f, 0.0f));
+        new Missile(GetGame(), mPosition, 100.0f, 5.0f, "../Assets/Sprites/Chaser/roc.png", Vector3(1.0f, 0.5f, 0.0f));
         SDL_Log("Chaser fired normal missile!");
         mMissileFireTimer = 0.0f;
     }
@@ -153,7 +153,7 @@ void Chaser::UpdateBattle(float deltaTime)
     if (mMissileFireTimer >= mBattleMissileFireRate)
     {
         // Battle missile
-        new Missile(GetGame(), mPosition, 250.0f, 5.0f, "../Assets/Sprites/Chaser/storm_shadow.png", Vector3(1.0f, 0.0f, 0.5f));
+        new Missile(GetGame(), mPosition, 150.0f, 2.5f, "../Assets/Sprites/Chaser/storm_shadow.png", Vector3(1.0f, 0.0f, 0.5f));
         SDL_Log("Chaser fired fast battle missile!");
         mMissileFireTimer = 0.0f;
     }
